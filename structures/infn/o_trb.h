a47ea84abebb29c3a9a477ad507ff19e8d05dc4e
andrea nardinocchi <andrea@nardinan.it>
2013-11-04 12:01:48 +0100

    New define d_trb_vas (number of VAs in the board)

--- structures/infn/o_trb.h
+++ structures/infn/o_trb.h
@@ -26,6 +26,7 @@
 #define d_trb_read_endpoint 1
 #define d_trb_channels 384
 #define d_trb_channels_half 192
+#define d_trb_vas 6
 #define d_trb_buffer_size 1560
 #define d_trb_event_size_normal 780
 #define d_trb_event_size_debug 112
